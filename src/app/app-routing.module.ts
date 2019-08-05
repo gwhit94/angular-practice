bf5bfeb3d8bce2fee4e15c8740fd461a9ed5de20
Graham <greysnaa@gmail.com>
2019-08-05 16:24:17 -0500

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { RandomNumberComponent } from './random-number/random-number.component';
+import { NamesListComponent } from './names-list/names-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'first', component: FirstComponent},
+  {path: 'second', component: SecondComponent},
+  {path: 'random', component: RandomNumberComponent},
+  {path: 'names', component: NamesListComponent},
+  {path: '**', redirectTo: '/first'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
